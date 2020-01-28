8fd23a050520d00add9501ebaa42128afee1face
pearlsoflutra5 <pearlsoflutra5@gmail.com>
2020-01-28 07:32:06 -0700

    fix files

--- Ch 19/Exercise19point3.java
+++ Ch 19/Exercise19point3.java
@@ -1,5 +1,25 @@
-class Exercise19point3 {
-	public static void main(String[] args) {
-		
-	}
+/*
+Author: Kacie Rae
+Date: 1-28-20
+
+Description: 
+ */
+import java.util.ArrayList;
+
+public class Exercise19_03 {
+  public static void main(String[] args) {
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    list.add(14);
+    list.add(24);
+    list.add(14);
+    list.add(42);
+    list.add(25);
+    
+    ArrayList<Integer> newList = removeDuplicates(list);
+    
+    System.out.print(newList);
+  }
+  public static <E> ArrayList<E>removeDuplicates (ArrayList<E> list){
+      
+  }
 }
