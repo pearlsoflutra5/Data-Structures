8e959412bce5322cdd7ca838cb1ae1cce86a4a34
pearlsoflutra5 <pearlsoflutra5@gmail.com>
2020-01-28 07:37:11 -0700

    Update Exercise19point3.java

--- Ch 19/Exercise19point3.java
+++ Ch 19/Exercise19point3.java
@@ -6,7 +6,7 @@ Description:
  */
 import java.util.ArrayList;
 
-public class Exercise19_03 {
+public class Exercise19point3 {
   public static void main(String[] args) {
     ArrayList<Integer> list = new ArrayList<Integer>();
     list.add(14);
@@ -14,12 +14,12 @@ public class Exercise19_03 {
     list.add(14);
     list.add(42);
     list.add(25);
-    
     ArrayList<Integer> newList = removeDuplicates(list);
-    
     System.out.print(newList);
   }
-  public static <E> ArrayList<E>removeDuplicates (ArrayList<E> list){
-      
+  
+  public static <E> ArrayList<E>removeDuplicates (ArrayList<E> list) {
+      if(list)
   }
+  
 }
