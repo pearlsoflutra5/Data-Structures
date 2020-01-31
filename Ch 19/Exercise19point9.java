906a5d3d7c160d2ecb2f30737ad49e51bb9f0c62
pearlsoflutra5 <student@MacBook-Pro-5.local>
2020-01-31 07:18:04 -0700

    Update Exercise19point9.java

--- Ch 19/Exercise19point9.java
+++ Ch 19/Exercise19point9.java
@@ -16,6 +16,19 @@ public class Exercise19point9 {
 		System.out.print(list);
 	}
 	public static <E extends Comparable<E>> void sort(ArrayList<E> list) { 
-		
+		for (int i = 0; i < list.size() - 1; i++) {
+			E currentMin = list.get(i);
+			int min = i;
+			for (int j = i + 1; j < list.size(); j++) {
+				if (list.get(j).compareTo(currentMin) < 0) {
+					currentMin = list.get(j);
+					min = j;
+				}
+			}
+			if (min != i) {
+				list.set(min, list.get(i));
+				list.set(i, currentMin);
+			}
+		}
 	}
 }
