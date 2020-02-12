aa3d4b72fdf78b940e3cd1f8e6c003aa6bfe100b
Pearlsoflutra5 <pearlsoflutra5@gmail.com>
2020-02-12 07:37:55 -0700

    Update Exercise21point7.java

--- Ch 21/Exercise21point7.java
+++ Ch 21/Exercise21point7.java
@@ -9,7 +9,7 @@ public class Exercise21point7 {
 	public static void main(String[] args) {
 		String text = "Good morning. Have a good class. " + "Have a good visit. Have fun!";
 		
-		Map<String, Integer> map = new TreeMap<>();
+		Map<String, Integer> map = new HashMap<>();
 		String[] words = text.split("[\\s+\\p{P}]");
 		for (int i = 0; i < words.length; i++) {
 			String key = words[i].toLowerCase();
