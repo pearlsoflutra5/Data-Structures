5a3febd699c07df10346f13d270d928232a9b856
pearlsoflutra5 <40216815+pearlsoflutra5@users.noreply.github.com>
2020-02-18 07:19:50 -0700

    Added Ch 22 files

---
+++ Ch 22/Exercise22point3.java
@@ -0,0 +1,42 @@
+/*
+Author: Kacie Rae
+Date: 2-18-20
+Description: Checking two strings to see if the second one is a substring of the first.
+*/
+import java.util.*;
+
+public class Exercise22point3 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter a string s1: ");
+		String s1 = input.nextLine();
+		System.out.print("Enter a string s2: ");
+		String s2 = input.nextLine();
+
+		int index = -1;
+		int count = 0; 
+
+		for (int i = 0; i < s1.length(); i++) {
+			if (s1.charAt(i) == s2.charAt(0) && count == 0) {
+				index = i;
+				count++;
+			}
+			else if (s1.charAt(i) == s2.charAt(count)) {
+				count++;
+			}
+			else {
+				count = 0;
+				index = -1;
+			}
+			if (count == s2.length()){
+				break;
+			}
+		}
+
+		if (index > 1)
+			System.out.println("matched at index " + index);
+		else
+			System.out.println("string2 is not a substring of string1");
+	}
+}
